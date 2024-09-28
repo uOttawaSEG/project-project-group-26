6507eee316e872251f89315e3b34795f2a1339c3
James Malek <jammalek@hotmail.com>
2024-09-28 18:15:17 -0400

    First Commits

---
+++ Person.java
@@ -0,0 +1,47 @@
+public class Person{
+    private String firstName, lastName;
+    private String phoneNumber;
+    private String address;
+
+    public Person(String firstName, String lastName, String address, String phoneNumber){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.phoneNumber=phoneNumber;
+        this.address=address;
+        
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName=firstName;
+    }
+
+    public String getLastNameString(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName=lastName;
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber=phoneNumber;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public void setAddress(String address){
+        this.address=address;
+    }
+
+
+}
