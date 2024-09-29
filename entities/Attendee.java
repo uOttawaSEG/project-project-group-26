a11c846f63650bc9998e8428dba625f380892f3d
MelanieMalekk <meljnmalek@gmail.com>
2024-09-29 16:49:46 -0400

    added README

--- entities/Attendee.java
+++ entities/Attendee.java
@@ -6,7 +6,6 @@ public class Attendee extends Person{
     public Attendee(String firstName, String lastName, String address, String phoneNumber, String emailAddress){
         super(firstName, lastName, address, phoneNumber);
         this.emailAddress=emailAddress;
-
     }
 
     public String getEmailAddress(){
