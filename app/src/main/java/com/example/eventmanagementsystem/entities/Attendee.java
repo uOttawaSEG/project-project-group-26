0c72605c5d48c08522c14644f56a1b05156ef830
James Malek <jammalek@hotmail.com>
2024-09-30 13:14:50 -0400

    Androi Commit

--- app/src/main/java/com/example/eventmanagementsystem/entities/Attendee.java
+++ app/src/main/java/com/example/eventmanagementsystem/entities/Attendee.java
@@ -6,6 +6,7 @@ public class Attendee extends Person{
     public Attendee(String firstName, String lastName, String address, String phoneNumber, String emailAddress){
         super(firstName, lastName, address, phoneNumber);
         this.emailAddress=emailAddress;
+
     }
 
     public String getEmailAddress(){
