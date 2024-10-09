c066d0ff02a55a8a0d2732f4f7f5138a9b22d143
JustinEl-Chibani <145512671+JustinEl-Chibani@users.noreply.github.com>
2024-10-08 23:16:26 -0400

    wrote a simple log off method, but need to rego over because its too simple to be honest, does the job for now

--- app/src/main/java/com/example/eventmanagementsystemems/entities/User.java
+++ app/src/main/java/com/example/eventmanagementsystemems/entities/User.java
@@ -94,5 +94,21 @@ public abstract class User extends Person {
 
     public abstract void requestRegistration();
 
-    public void logOff(){}
+    public void logOff(){
+        /*This is the simplest form of logoff I could think of. It's not enough though retalk with everyone 
+        because there's no way we could have a logoff method written in the user class because the user class 
+        is meant to just store information about a user, where as logging off will need to redirect people to 
+        another page and clear their data in the database. Hala2 keep it for now but once the button and everything 
+        is in i think we should move it to another class which manages the users session 
+        (ex: signing in, redirecting to other pages, etc.)*/
+
+        //Nullify the values of the instance to represent that the user has been logged out
+        this.firstName = null;
+        this.lastName = null;
+        this.phoneNumber = null;
+        this.emailAddress = null;
+        this.password = null;
+
+        System.out.println("User has been logged off.");
+    }
 }
