11c6437b15e97bef5c29a3ce5430dac256ac8299
James Malek <jmale047@uottawa.ca>
2024-10-16 09:44:21 -0400

    LogOff Functionality Works

--- app/src/main/java/com/example/eventmanagementsystemems/WelcomeScreen.java
+++ app/src/main/java/com/example/eventmanagementsystemems/WelcomeScreen.java
@@ -2,15 +2,22 @@ package com.example.eventmanagementsystemems;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.eventmanagementsystemems.accounts.logoff.LogoffActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class WelcomeScreen extends AppCompatActivity {
 
     private TextView tvWelcomeMessage;
     private String firstName;
-
+    private Button btnLogoff;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +29,21 @@ public class WelcomeScreen extends AppCompatActivity {
         if (userType != null) {
             tvWelcomeMessage.setText("Welcome, " + userType + "!");
         }
+        btnLogoff = findViewById(R.id.btnLogoff);
+        btnLogoff.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                FirebaseAuth.getInstance().signOut();
+
+                Toast.makeText(WelcomeScreen.this, "Logged off succesfully!", Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(WelcomeScreen.this, MainActivity.class);
+                //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+
         // Get the first name passed from LoginActivity
 //        firstName = getIntent().getStringExtra("FIRST_NAME");
 
@@ -32,4 +54,6 @@ public class WelcomeScreen extends AppCompatActivity {
 //            tvWelcomeMessage.setText("Welcome to EMS!");
 //        }
     }
+
+
 }
