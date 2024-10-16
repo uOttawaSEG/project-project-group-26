e44d7733b7ac339bffa6a595e978724641f9758b
Ralph Nabhan <nabhanralph@gmail.com>
2024-10-16 16:19:13 -0400

    3 welcome pages for diffrent roles

--- app/src/main/java/com/example/eventmanagementsystemems/WelcomeScreen.java
+++ app/src/main/java/com/example/eventmanagementsystemems/WelcomeScreen.java
@@ -12,7 +12,7 @@ public class WelcomeScreen extends AppCompatActivity {
     private String firstName;
 
     @Override
-    protected void  onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
 
