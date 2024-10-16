10dd8fe158f527d27c545066961733716598b493
JustinEl-Chibani <145512671+JustinEl-Chibani@users.noreply.github.com>
2024-10-16 16:10:00 -0400

    extended

--- app/src/main/java/com/example/eventmanagementsystemems/accounts/logoff/LogoffActivity.java
+++ app/src/main/java/com/example/eventmanagementsystemems/accounts/logoff/LogoffActivity.java
@@ -1,12 +1,12 @@
 package com.example.eventmanagementsystemems.accounts.logoff;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.eventmanagementsystemems.R;
 import com.example.eventmanagementsystemems.accounts.login.LoginActivity;
