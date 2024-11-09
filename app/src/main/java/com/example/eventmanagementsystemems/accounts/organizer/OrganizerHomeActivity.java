96fed3f8de29b0f4c3b7dc2231860dc7a9bed6dc
Chafic Nehme <cnehm006@uottawa.ca>
2024-11-09 00:39:01 -0500

    forgot an import statement :p

--- app/src/main/java/com/example/eventmanagementsystemems/accounts/organizer/OrganizerHomeActivity.java
+++ app/src/main/java/com/example/eventmanagementsystemems/accounts/organizer/OrganizerHomeActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.eventmanagementsystemems.CreateEventActivity;
 import com.example.eventmanagementsystemems.R;
 import com.example.eventmanagementsystemems.ViewEventsActivity;
 
