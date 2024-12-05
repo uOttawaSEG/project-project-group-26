5add5bcb3f4d4f71d0f360f6dbfd2985ab26a0d4
lourdes <lourdes.najjar8@gmail.com>
2024-12-04 19:51:18 -0500

    espresso imports

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,5 +44,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
+
 
 }
